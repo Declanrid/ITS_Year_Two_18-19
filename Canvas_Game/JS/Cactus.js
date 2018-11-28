72473662c9bcbaf5d67358b027f41f15177ef887
huegort <andrew.beatty@gmit.ie>
2018-11-28 10:52:19 +0000

    Work

--- Canvas_Game/JS/Cactus.js
+++ Canvas_Game/JS/Cactus.js
@@ -5,7 +5,9 @@ function Cactus (canvas) {
     this.y          = 440;
     this.sx         = 158;
     this.sy         = 240;
-    this.vx         = -Math.random() * 15 - 5;
+    this.w          = 50;
+    this.h          = 70;
+
     this.ready      = false;
     this.imageUrl   = "Images/Cactus_158x240.png";
     this.baseImage  = new Image();
@@ -19,17 +21,17 @@ function Cactus (canvas) {
     this.baseImage.src = this.imageUrl;
 }
 
-
+Cactus.prototype.vx = -5;
 Cactus.prototype.draw= function (ctx) {
     if (this.ready) {
-        ctx.drawImage(this.baseImage, 0, 0, this.sx, this.sy, this.x, this.y, 50, 70);
+        ctx.drawImage(this.baseImage, 0, 0, this.sx, this.sy, this.x, this.y, this.w, this.h);
     console.log(this.vx)
     }
 };
 
 Cactus.prototype.tick= function (canvas) {
-    this.x += this.vx
-    return (this.x< 0);
+    this.x += this.vx;
+    return (this.x < 0);
 };
 
 
--- Canvas_Game/JS/wolfSprite.js
+++ Canvas_Game/JS/wolfSprite.js
@@ -1,5 +1,5 @@
 var wolf = {
-
+    game:           {},
     x:              350,
     y:              170,
     vx:             0,
@@ -17,7 +17,7 @@ var wolf = {
     keyJump:        38,
 
     draw: function (ctx) {
-        if (this.ready){
+            if (this.ready){
             if (this.x > canvas.width - this.sprites[this.currentSprite].sw) {this.x = canvas.width - this.sprites[this.currentSprite].sw}
             if (this.x < 0) {this.x = 0}
                 ctx.drawImage(this.baseImage,
@@ -36,7 +36,7 @@ var wolf = {
     },
 
     tick: function (canvas) {
-        this.currentSprite++;
+            this.currentSprite++;
 
             if (this.currentSprite == 5) {this.currentSprite = 0; }
             if (this.y > options.floor){
@@ -48,32 +48,52 @@ var wolf = {
         }
             this.x += this.vx;
             this.y += this.vy;
+            for (var i=0; i< game.obstacles.length; i++){
+                this.checkCollision(game.obstacles[i]);
+            }
 
     },
 
     initialise: function () {
 
-        this.baseImage.onload = function () {
-            var spritew = 64;
-            var spriteh = 32;
-            var row =4;
-            for (var col = 0; col < 5; col++) {
-                    wolf.sprites[col] = {};
-                    wolf.sprites[col].sx    = 320 + col * spritew;
-                    wolf.sprites[col].sy    = row * spriteh;
-                    wolf.sprites[col].sw    = 64;
-                    wolf.sprites[col].sh    = 32;
-                    wolf.x                  = 350;
-                    wolf.y                  = 450;
+                this.baseImage.onload = function () {
+                    var spritew = 64;
+                    var spriteh = 32;
+                    var row =4;
+                    for (var col = 0; col < 5; col++) {
+                        wolf.sprites[col] = {};
+                        wolf.sprites[col].sx    = 320 + col * spritew;
+                        wolf.sprites[col].sy    = row * spriteh;
+                        wolf.sprites[col].sw    = 64;
+                        wolf.sprites[col].sh    = 32;
+                        wolf.x                  = 350;
+                        wolf.y                  = 450;
 
             }
-
 
             wolf.ready = true;
 
         };
         this.baseImage.src = this.imageUrl;
     },
+    checkCollision:function ( otherActor) {
+        var wolfx       = this.x;
+        var wolfy       = this.y;
+        var wolfw       = this.sprites[this.currentSprite].sw;
+        var wolfh       = this.sprites[this.currentSprite].sh;
+        var cactusx     = otherActor.x;
+        var cactusy     = otherActor.y;
+        var cactusw     = otherActor.w;
+        var cactush     = otherActor.h;
+
+        if ((wolfx > cactusx - wolfw)&&
+            (wolfy > cactusy - wolfh)&&
+            (wolfx < cactusx + cactusw)&&
+            (wolfy < cactusy + cactush)){
+        }
+    },
+
+
     keyDown:function(keyCode){
         if (keyCode == this.keyRight)   {this.vx = 5;}
         if (keyCode == this.keyLeft)    {this.vx = -5;}
